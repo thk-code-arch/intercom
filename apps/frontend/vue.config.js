d78262105b8692e3c201fa35a25f3015f25e8de4
Steffen Stein <therealstein@tutanota.com>
2024-03-20 20:54:26 -0400

    <startup>:

--- apps/frontend/vue.config.js
+++ apps/frontend/vue.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   configureWebpack: {
     devServer: {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-      disableHostCheck: true
-    }
-  }
-}
-
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      disableHostCheck: true,
+    },
+  },
+  chainWebpack: (config) => {
+    // Exclude `node_modules` from processing by PostCSS
+    config.module.rule('css').exclude.add(/node_modules/);
+    config.module.rule('vue').exclude.add(/node_modules/);
+  },
+};
